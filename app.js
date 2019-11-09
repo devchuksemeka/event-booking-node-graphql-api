d94ca50a41a5ab0a1d9b77c5d6142c200e19bde7
Miracle Okafor <miracleokafor@christians-MacBook-Pro.local>
2019-11-09 18:03:15 +0100

    implemented user and event model relationship and also updates the logic

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const bodyParser = require('body-parser')
 const graphqlHttp = require('express-graphql')
 const { buildSchema } = require('graphql')
 const mongoose = require("mongoose")
+const bcrypt = require("bcryptjs")
 
 const Event = require('./models/event')
+const User = require('./models/user')
+
 const app = express();
 
 app.use(bodyParser.json());
@@ -22,6 +25,12 @@ app.use('/graphql-api',graphqlHttp({
             date: String!
         }
 
+        type User {
+            _id: ID!
+            email: String!
+            password: String 
+        }
+
         input EventInput {
             title:String!
             description: String!
@@ -29,12 +38,19 @@ app.use('/graphql-api',graphqlHttp({
             date: String!
         }
 
+        input UserInput {
+            email: String!
+            password: String!
+        }
+
         type RootQuery {
             events: [Event!]!
+
         }
 
         type RootMutation {
             createEvent(eventInput: EventInput): Event
+            createUser(userInput: UserInput): User
         }
 
         schema{
@@ -59,20 +75,49 @@ app.use('/graphql-api',graphqlHttp({
                 title: args.eventInput.title,
                 description: args.eventInput.description,
                 price: +args.eventInput.price,
-                date: new Date(args.eventInput.date)
+                date: new Date(args.eventInput.date),
+                creator: '5dc6e5a67a23f35292337175',
             })
+            let createdEvent;
             return event.
                 save()
                 .then(result=>{
-                    console.log(result);
-                    return {
-                        ...result._doc
-                    };
+                    createdEvent = { ...result._doc }; 
+                    return User.findById('5dc6e5a67a23f35292337175')
+                })
+                .then(user=>{
+                    if(!user) throw new Error("User does not exist")
+                    user.createdEvents.push(event);
+                    return user.save()
+                })
+                .then(result=>{
+                    return createdEvent;
                 })
                 .catch(err =>{
                     console.log(err)
                     throw err;
                 })
+        },
+        createUser: args =>{
+            return User.findOne({email:args.userInput.email})
+            .then(user => {
+                if(user) throw new Error('User exists already')
+                return bcrypt.hash(args.userInput.password,12)
+            })
+            .then(hashPassword=>{
+                const user = new User({
+                    email:args.userInput.email,
+                    password:hashPassword
+                })
+                return user.save()
+            })
+            .then(result => {
+                return {...result._doc,password:null}
+            })
+            .catch(err=>{
+                throw err
+            })
+            
         }
     },
     graphiql:true
